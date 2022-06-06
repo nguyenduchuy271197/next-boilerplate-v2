8408e1723ded9b5a28a6f43acb68f8c79002b74c
Huy Nguyen <nguyenduchuy271197@gmail.com>
2022-06-06 16:07:42 +0700

    Add git hooks

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.eslint.json',
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended', // eslint-config-prettier + eslint-plugin-prettier
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint', '@next/eslint-plugin-next'],
+  rules: {
+    'prettier/prettier': 'error', // Eslint rules
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+const NavBar = (): JSX.Element => {
+  return <div>NavBar</div>
+}
+
+export default NavBar
