9de043501c7d304372a42d0854d5c66f5d3b0ae9
Huy Nguyen <nguyenduchuy271197@gmail.com>
2022-06-06 16:16:40 +0700

    test husky, lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '@components/Footer'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -54,6 +55,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
+        <Footer />
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
